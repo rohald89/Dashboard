9752ea4605e40bdf8b71779e1606d2678fe24dc1
Rohald van Merode <rohald_89@live.nl>
2022-01-07 12:34:19 +0100

    added custom data

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -9,7 +9,7 @@ import {
     Legend,
   } from 'chart.js';
 import { Line } from 'react-chartjs-2';
-import faker from 'faker';
+// import faker from 'faker';
 
 
 ChartJS.register(
@@ -26,32 +26,57 @@ ChartJS.register(
     responsive: true,
     plugins: {
       legend: {
-        position: 'top',
+        // position: 'top',
+        display: false,
       },
-      title: {
-        display: true,
-        text: 'Chart.js Line Chart',
-      },
+    //   title: {
+    //     display: true,
+    //     text: 'Chart.js Line Chart',
+    //   },
     },
   };
   
-  const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  const labels = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "October",
+    "November",
+    "December",
+  ];
   
   export const data = {
     labels,
     datasets: [
       {
         label: 'Dataset 1',
-        data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-        borderColor: 'rgb(255, 99, 132)',
+        data: [
+            2000,
+            1200,
+            3950,
+            2950,
+            5600,
+            4050,
+            1950,
+            4450,
+            4850,
+            4900,
+            6100,
+          ],
+        borderColor: 'rgb(219,77,96)',
         backgroundColor: 'rgba(255, 99, 132, 0.5)',
       },
-      {
-        label: 'Dataset 2',
-        data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-        borderColor: 'rgb(53, 162, 235)',
-        backgroundColor: 'rgba(53, 162, 235, 0.5)',
-      },
+    //   {
+    //     label: 'Dataset 2',
+    //     data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+    //     borderColor: 'rgb(53, 162, 235)',
+    //     backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    //   },
     ],
   };
 const LineChart = () => {
